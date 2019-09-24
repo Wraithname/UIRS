b52ca3903869a13487022edee969ab214bf1ddd0
Wraithname <43811054+Wraithname@users.noreply.github.com>
2019-09-24 11:24:06 +0300

    Revert "Revert "Матричная форма""
    This reverts commit e4305a84c305b6684fa52fae50d890b70abbf371.

---
+++ UIRS/WorkWithStl/WorkWithStl/WorkWithStl/MainWindow.xaml.cs
@@ -0,0 +1,76 @@
+﻿using HelixToolkit.Wpf;
+using System;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media.Media3D;
+using Microsoft.Win32;
+using System.Windows.Media;
+using STL_Tools;
+using System.Collections.Generic;
+
+namespace WorkWithStl
+{ /// <summary>
+  /// Interaction logic for MainWindow.xaml
+  /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        /// <summary>
+        /// Display 3D Model
+        /// </summary>
+        /// <param name="model">Path to the Model file</param>
+        /// <returns>3D Model Content</returns>
+        private Model3D Display3d(string model)
+        {
+            Model3D device = null;
+            try
+            {
+                //Adding a gesture here
+                viewPort3d.RotateGesture = new MouseGesture(MouseAction.LeftClick);
+
+                //Import 3D model file
+                ModelImporter import = new ModelImporter();
+                Material material = new DiffuseMaterial(new SolidColorBrush(Colors.Silver));
+                //Load the 3D model file
+                import.DefaultMaterial = material;
+                device = import.Load(model);
+               
+            }
+            catch (Exception e)
+            {
+                // Handle exception in case can not file 3D model
+                MessageBox.Show("Exception Error : " + e.StackTrace);
+            }
+            return device;
+        }
+private void OpenFile(object sender, RoutedEventArgs e)
+        {
+           
+            string FilePath=null; 
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if(openFileDialog.ShowDialog()==true)
+            {
+                FilePath = openFileDialog.FileName;
+                STLReader stlReader = new STLReader(openFileDialog.FileName);
+                TriangleMesh[] meshArray = stlReader.ReadFile();
+                ModelVisual3D device3D = new ModelVisual3D();
+                device3D.Content = Display3d(FilePath);
+                viewPort3d.Children.Clear();
+                // Добавление в порт
+               
+                viewPort3d.Children.Add(device3D);
+                viewPort3d.Children.Add(new SunLight());
+                viewPort3d.ZoomExtents();
+                
+            }
+        }
+
+        private void Quit(object sender, RoutedEventArgs e)
+        {
+            Application.Current.MainWindow.Close();
+        }
+    }
+}
